298d3cee99aac8b4b9ccf92b19ef77d913220888
hitheshCC <hithesh.v@codecraft.co.in>
2022-06-03 11:03:20 +0530

    Update MainActivity.kt

--- DumDumDummy/app/src/main/java/com/example/dumdumdummy/MainActivity.kt
+++ DumDumDummy/app/src/main/java/com/example/dumdumdummy/MainActivity.kt
@@ -7,6 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Hello hello bitrise")
+        println(add(4, 5))
+       
+    }
+    
+    fun add(a: Int, b: String): Int {
+        return a + b.toInt()
     }
 }
